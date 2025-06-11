23f0fe21cf9e1ed6edb55b00911aa1aa75d73c13
Abdallah mohamed <136760756+AbdallahMohamedd@users.noreply.github.com>
2025-06-11 14:13:11 +0300

    (Lab 1) --> First order Transfer Function in .m file

--- Module_4 PID Controller Design/lab_1/01_TF_First_Order/First_Order.m
+++ Module_4 PID Controller Design/lab_1/01_TF_First_Order/First_Order.m
@@ -4,11 +4,15 @@
 % It specifies the system parameters, including gain, resistance, and capacitance.
 % The transfer function is then created using these parameters.
 
-k = 1       ; % Gain
-R = 10e3    ; % Resistance
-C = 1e-7    ; % Capacitance
-T = R*C     ; % Time constant
-num = k     ; % Numerator coefficients
-den = [T 1] ; % Denominator coefficients
-G = tf(num, den)  % Transfer function
+k = 1;                  % Gain
+R = 10e3;               % Resistance
+C = 1e-7;               % Capacitance
+T = R*C;                % Time constant
+num = k;                % Numerator coefficients
+for index = 0:0.25:1
+    den = [index 1] ;   % Denominator coefficients
+    G = tf(num, den)    % Transfer function
+    hold on;
+    step(G);           % Step response
+end
 % G = tf([k],[T 1]); % Alternative way to define transfer function
