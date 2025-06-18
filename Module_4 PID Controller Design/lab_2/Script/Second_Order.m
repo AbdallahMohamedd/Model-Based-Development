06ec0bfdd00b241c8ec512ba310c190aefc92529
Abdallah mohamed <136760756+AbdallahMohamedd@users.noreply.github.com>
2025-06-18 09:32:17 +0300

    (Lab 2) --> Second order Transfer Function in .m file

--- Module_4 PID Controller Design/lab_2/Script/Second_Order.m
+++ Module_4 PID Controller Design/lab_2/Script/Second_Order.m
@@ -17,7 +17,23 @@ for zeta = 0.25:0.25:1.5
     step(G); % Step response of the system
     legend('zeta = 0.25', 'zeta = 0.5', 'zeta = 0.75', 'zeta = 1', 'zeta = 1.25');
     % Pole-zero map of the system
-    title('step response of a second-order system with varying damping ratios (zeta).');
+    title('step response of a second-order system with varying damping ratios (zeta) and constant natural frequencies.');
 end
+
+figure
+% Define the Natural frequency
+for wn = 0.75:0.5:2.75
+    % Define the transfer function of the second-order system
+    zeta = 0.5;   % Damping ratio
+    % Transfer function of the second-order system
+    num = wn^2; % Numerator coefficients
+    den = [1 2*zeta*wn wn^2]; % Denominator coefficients
+    G = tf(num, den); % Transfer function
+    hold on;
+    step(G); % Step response of the system
+    legend('wn = 0.75', 'wn = 1.25', 'wn = 1.75', 'wn = 2.25', 'wn = 2.75');
+    % Pole-zero map of the system
+    title('step response of a second-order system with varying natural frequencies (wn) and constant zeta.');
+end
+
 clc
-    G = tf(num, den) % Transfer function
