381a1de497dc654f375d4c59e9af7d751ab09edc
Abdallah mohamed <136760756+AbdallahMohamedd@users.noreply.github.com>
2025-06-18 09:18:12 +0300

    (Lab 2) --> Second order Transfer Function in .m file

---
+++ Module_4 PID Controller Design/lab_2/Script/Second_Order.m
@@ -0,0 +1,22 @@
+%info
+% This script generates the step response of a second-order system with varying damping ratios (zeta).
+% It uses the Control System Toolbox in MATLAB to create transfer functions and plot the step responses.
+
+clc
+clear
+close all
+% Define the damping ratios
+for zeta = 0.25:0.25:1.5
+    % Define the transfer function of the second-order system
+    wn = 0.5;   % Natural frequency
+    % Transfer function of the second-order system
+    num = wn^2; % Numerator coefficients
+    den = [1 2*zeta*wn wn^2]; % Denominator coefficients
+    G = tf(num, den); % Transfer function
+    hold on;
+    step(G); % Step response of the system
+    legend('zeta = 0.25', 'zeta = 0.5', 'zeta = 0.75', 'zeta = 1', 'zeta = 1.25');
+    % Pole-zero map of the system
+    title('step response of a second-order system with varying damping ratios (zeta).');
+end
+clc
